d22a7de26d56cca2e5e541b63f0d2f121671cf15
bonomjy07 <bonomjy07@naver.com>
2020-08-09 00:39:35 +0900

    Init

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public const string fireButtonName = "Fire1";
+    public const string jumpButtonName = "Jump";
+    public const string moveHorizontalAxisName = "Horizontal";
+    public const string moveVerticalAxisName = "Vertical";
+    public const string reloadButtonName = "Reload";
+
+    public Vector2 moveInput { get; private set; }
+    public bool fire { get; private set; }
+    public bool reload { get; private set; }
+    public bool jump { get; private set; }
+    
+    private void Update()
+    {
+        if (GameManager.Instance != null && GameManager.Instance.isGameover)
+        {
+            moveInput = Vector2.zero;
+            fire = false;
+            reload = false;
+            jump = false;
+            return;
+        }
+
+        moveInput = new Vector2(Input.GetAxis(moveHorizontalAxisName), Input.GetAxis(moveVerticalAxisName));
+        if (moveInput.sqrMagnitude > 1) moveInput = moveInput.normalized;
+
+        jump = Input.GetButtonDown(jumpButtonName);
+        fire = Input.GetButton(fireButtonName);
+        reload = Input.GetButtonDown(reloadButtonName);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController characterController;
+    private PlayerInput playerInput;
+    private Animator animator;
+    
+    private Camera followCam;
+    
+    public float speed = 6f;
+    public float jumpVelocity = 20f;
+    [Range(0.01f, 1f)] public float airControlPercent;
+
+    public float speedSmoothTime = 0.1f;
+    public float turnSmoothTime = 0.1f;
+    
+    private float speedSmoothVelocity;
+    private float turnSmoothVelocity;
+    
+    private float currentVelocityY;
+
+    // expression-bodied member
+    public float currentSpeed =>
+        new Vector2(characterController.velocity.x, characterController.velocity.z).magnitude;
+    
+    private void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+        followCam = Camera.main;
+    }
+
+    // Physical relevant tasks
+    private void FixedUpdate()
+    {
+        if (currentSpeed > 0.2f || playerInput.fire) Rotate();
+
+        Move(playerInput.moveInput);
+        
+        if (playerInput.jump) Jump();
+    }
+
+    // Animation relevant tasks
+    private void Update()
+    {
+        UpdateAnimation(playerInput.moveInput);
+    }
+
+    public void Move(Vector2 moveInput)
+    {
+        // Speed with damp
+        float targetSpeed = speed * moveInput.magnitude;
+        float smoothTime = characterController.isGrounded ? (speedSmoothTime) : (speedSmoothTime / airControlPercent);
+        targetSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, smoothTime);
+
+        // Velocity
+        Vector3 direction = Vector3.Normalize(transform.forward * moveInput.y + transform.right * moveInput.x);
+        currentVelocityY += Time.deltaTime * Physics.gravity.y;
+        Vector3 currentVelocity = (direction * targetSpeed) + (Vector3.up * currentVelocityY);
+
+        // Move
+        characterController.Move(currentVelocity * Time.deltaTime);
+
+        // 
+        if (characterController.isGrounded) currentVelocityY = 0f;
+    }
+
+    public void Rotate()
+    {
+        float targetRotation = followCam.transform.eulerAngles.y;
+        targetRotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
+
+        transform.eulerAngles = Vector3.up * targetRotation;
+    }
+
+    public void Jump()
+    {
+        if (!characterController.isGrounded)
+        {
+            return;
+        }
+
+        currentVelocityY = jumpVelocity;
+    }
+
+    private void UpdateAnimation(Vector2 moveInput)
+    {
+        float animationSpeedPercent = currentSpeed / speed;
+        animator.SetFloat("Vertical Move", moveInput.y * animationSpeedPercent, 0.05f, Time.deltaTime);
+        animator.SetFloat("Horizontal Move", moveInput.x * animationSpeedPercent, 0.05f, Time.deltaTime);
+    }
+}
